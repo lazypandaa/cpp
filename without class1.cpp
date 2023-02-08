a6f5aa413bb00d4609e1f8bb7c6b5a5c00c42cfd
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2023-02-08 06:52:55 +0530

    Add files via upload

---
+++ without class1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//hi
+int main()
+{
+    int ID; 
+    char Name[10];
+        cout<<"Enter ID: ";
+        cin>>ID;
+        cout<<"Enter Name: ";
+        cin>>Name;
+        cout<<"you are : "<<ID<<" "<<Name;
+    return 0;
+}
